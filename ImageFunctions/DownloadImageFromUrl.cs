2aee128716e65cca4bd26702a85fd6acabbc61e5
Henry Senior <henry.senior@carfinance247.co.uk>
2020-09-30 11:34:56 +0100

    started download image from URL function

---
+++ ImageFunctions/DownloadImageFromUrl.cs
@@ -0,0 +1,45 @@
+using Azure.Storage.Blobs;
+using Microsoft.Azure.EventGrid.Models;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.EventGrid;
+using Microsoft.Extensions.Logging;
+using System;
+using System.IO;
+using System.Net;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ImageFunctions
+{
+    public class DownloadImageFromUrl
+    {
+        public static readonly string BLOB_STORAGE_CONNECTION_STRING = Environment.GetEnvironmentVariable("AzureWebJobsStorage");
+
+        [FunctionName("DownloadImageFromUrl")]
+        public static async Task DownloadImageFromUrl(string url, ILogger log)
+        {
+            try
+            {
+                var urlComponents = url.Split('.');
+                var extension = urlComponents[urlComponents.Length - 1];
+
+                // Check length is that of an extension (jpeg, jpg, png, bmp, etc)
+                if(extension.Length <= 4 && extension.Length >= 3)
+                {
+                    using(var wc = new WebClient())
+                    {
+                        await wc.DownloadFileTaskAsync(url, $"test.{extension}");
+                    }
+                }
+                else
+                {
+                    log.LogInformation("The url did not contain an extension");
+                }
+            }
+            catch (Exception ex)
+            {
+                log.LogError(ex.Message);
+            }
+        }
+    }
+}
